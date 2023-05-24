a245a379582438bca2d72c5bc9b918332889ef66
Naimul Ferdous <ferdousnaimul@gmail.com>
2023-05-24 18:37:04 +0600

    few corrections

--- src/Page/Contact/Contact.jsx
+++ src/Page/Contact/Contact.jsx
@@ -1,82 +1,170 @@
-import React, { useState } from 'react';
-import Footer from '../../Component/Footer/Footer';
+import axios from "axios";
+import { Formik } from "formik";
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import * as Yup from "yup";
+import Footer from "../../Component/Footer/Footer";
 import Banner from "../../Component/HomeBanner";
-import NavBar from '../../Component/NavBar/NavBar';
+import NavBar from "../../Component/NavBar/NavBar";
 import homeimage from "../../assets/image/coverHome.jpg";
 import "./Contact.css";
+const MySwal = withReactContent(Swal);
 
 export default function Contact() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    if (loading) {
+      MySwal.fire(<p>Feedback submitting</p>);
+    }
+  }, [loading]);
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  const contactSchema = Yup.object().shape({
+    name: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+    message: Yup.string()
+      .min(6, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+  });
 
-    // Make an HTTP POST request to your server here with the form data
-    // using Fetch API or a library like Axios
+  const handleContactSubmit = (values, actions) => {
+    actions.setSubmitting(true);
+    setLoading(true);
+    const regPostObj = {
+      name: values.name,
+      email: values.email,
+      message: values.message,
+    };
 
-    // Display a success message or an error message to the user
-  }
+    const regUrl = "https://feedback.bcsconfidence.net/api/send/mail/feedback";
+
+    axios
+      .post(regUrl, regPostObj)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(response);
+          actions.setSubmitting(false);
+          setLoading(false);
+
+          actions.resetForm();
+          MySwal.fire(<p>Feedback submitted successful</p>);
+        }
+      })
+      .catch((error) => {
+        console.log("errors", error);
+        actions.setSubmitting(false);
+        setLoading(false);
+
+        if (error.response.data.message.length) {
+          return MySwal.fire(<p>{error.response.data.message}</p>);
+        }
+      });
+  };
+
   return (
     <div className="">
-    <NavBar/>
-    <Banner image={homeimage} header={"BCS Confidence"} text={"BCS কনফিডেন্স ১৯৯১ সালে প্রতিষ্ঠিত চাকুরি প্রার্থীদের একটি সহায়ক প্রতিষ্ঠান"} title={"Lead By: Lion Taslima Gias"}/>
-    <div className="container">
-    <div className="row">
-      <div className="col-md-6">
-        <div className="card" style={{height:"540px",display:"flex",justifyContent:"center",alignItems:"center",padding:"25px",backgroundColor:"darkseagreen",fontSize:"1.2rem",margin:"20px"}}>
-          <p>
-          আমাদের সাথে যোগাযোগ করার জন্য ফর্মটি পূরণ করুন অথবা নিচে দেওয়া আমাদের নম্বরে যোগাযোগ করুন অথবা আমাদের ঠিকানায় এসে যোগাযোগ করুন।
-          </p>
+      <NavBar />
+      <Banner
+        image={homeimage}
+        header={"BCS Confidence"}
+        text={
+          "BCS কনফিডেন্স ১৯৯১ সালে প্রতিষ্ঠিত চাকুরি প্রার্থীদের একটি সহায়ক প্রতিষ্ঠান"
+        }
+        title={"Lead By: Lion Taslima Gias"}
+      />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div
+              className="card"
+              style={{
+                height: "540px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "25px",
+                backgroundColor: "darkseagreen",
+                fontSize: "1.2rem",
+                margin: "20px",
+              }}
+            >
+              <p>
+                আমাদের সাথে যোগাযোগ করার জন্য ফর্মটি পূরণ করুন অথবা নিচে দেওয়া
+                আমাদের নম্বরে যোগাযোগ করুন অথবা আমাদের ঠিকানায় এসে যোগাযোগ করুন।
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="contact-form">
+              <div>
+                <h1>Contact Us</h1>
+                <Formik
+                  initialValues={{
+                    name: "",
+                    email: "",
+                    message: "",
+                  }}
+                  validationSchema={contactSchema}
+                  onSubmit={handleContactSubmit}
+                >
+                  {({
+                    errors,
+                    touched,
+                    handleSubmit,
+                    handleChange,
+                    handleBlur,
+                    values,
+                  }) => (
+                    <form onSubmit={handleSubmit}>
+                      <label>
+                        Name:
+                        <input
+                          type="text"
+                          name="name"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.name}
+                        />
+                        {touched.name && errors.name && (
+                          <div style={{ color: "red" }}>{errors.name}</div>
+                        )}
+                      </label>
+                      <label>
+                        Email:
+                        <input
+                          type="email"
+                          name="email"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.email}
+                        />
+                        {touched.email && errors.email && (
+                          <div style={{ color: "red" }}>{errors.email}</div>
+                        )}
+                      </label>
+                      <label>
+                        Message:
+                        <textarea
+                          name="message"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.message}
+                        />
+                        {touched.message && errors.message && (
+                          <div style={{ color: "red" }}>{errors.message}</div>
+                        )}
+                      </label>
+                      <button type="submit">Submit</button>
+                    </form>
+                  )}
+                </Formik>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="col-md-6">
-      <div className='contact-form'>
-        <div>
-      <h1>Contact Us</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </label>
-        <label>
-          Subject:
-          <input
-            type="text"
-            value={subject}
-            onChange={(event) => setSubject(event.target.value)}
-          />
-        </label>
-        <label>
-          Message:
-          <textarea
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+      <Footer />
     </div>
-    </div>
-      </div>
-      </div>
-    
-    </div>
-    <Footer/>
-    </div>
-  )
+  );
 }
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <NavBar />
-      <Banner image={homeimage} header={"BCS Confidence"} text={"BCS কনফিডেন্স ১৯৯১ সালে প্রতিষ্ঠিত চাকুরি প্রার্থীদের একটি সহায়ক প্রতিষ্ঠান"} title={"Lead By: Lion Taslima Gias"}/>
+      <Banner image={homeimage} header={"স্বপ্ন যদি হয় BCS ক্যাডার, যথার্থ হোক সময়ের ব্যবহার"} text={"BCS কনফিডেন্স ১৯৯১ সালে প্রতিষ্ঠিত চাকুরি প্রার্থীদের একটি সহায়ক প্রতিষ্ঠান"} title={"Lead By: Lion Taslima Gias"}/>
       <h3 className="text-center m-3 ">আমাদের কার্যক্রম সমূহ</h3>
       <WeAre />
       <div className=""style={{backgroundColor:"rgb(249 249 249)",marginTop:"50px"}}>
